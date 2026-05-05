a35eca0ae42ba6fc4c4314e17e4b4e9e87999de1
gustavod25 <gustavodf25@gmail.com>
2026-05-05 00:51:25 -0300

    estructura principal

---
+++ Tp3/src/main/java/unlar/edu/ar/Tp3/Tp3Application.java
@@ -0,0 +1,16 @@
+package unlar.edu.ar.Tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Application.class, args);
+
+		System.out.println("Hola Mundo");
+
+	}
+
+}
